40b4ab5c79b13090dab84918ef2af07786a34cb1
ViniciusSantos19 <vinicontatosq@gmail.com>
2024-04-20 20:29:01 -0300

    adicione os plugins para java no lvim

--- config.lua
+++ config.lua
@@ -1,4 +1,3 @@
-
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
@@ -34,5 +33,36 @@ lvim.plugins = {
 			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
 		},
 	},
-	{"olimorris/onedarkpro.nvim"},
+	{ "olimorris/onedarkpro.nvim" },
+	{
+		"nvim-java/nvim-java",
+		dependencies = {
+			"nvim-java/lua-async-await",
+			"nvim-java/nvim-java-core",
+			"nvim-java/nvim-java-test",
+			"nvim-java/nvim-java-dap",
+			"MunifTanjim/nui.nvim",
+			"neovim/nvim-lspconfig",
+			"mfussenegger/nvim-dap",
+			{
+				"williamboman/mason.nvim",
+				opts = {
+					registries = {
+						"github:nvim-java/mason-registry",
+						"github:mason-org/mason-registry",
+					},
+				},
+			},
+			{
+				"williamboman/mason-lspconfig.nvim",
+				opts = {
+					handlers = {
+						["jdtls"] = function()
+							require("java").setup()
+						end,
+					},
+				},
+			},
+		},
+	},
 }
